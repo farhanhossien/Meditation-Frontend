2247a7b428a22a5094445b90c74e3e3d01ae00fc
Farhan Hossien <farhanhossien@example.com>
2026-05-14 23:24:43 +0600

    Initial frontend commit

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'stillness-v1';
+const ASSETS = [
+  './stillness-v2-1.html',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;1,300;1,400&family=Jost:wght@200;300;400&display=swap'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
